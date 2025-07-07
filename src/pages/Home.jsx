0007d1ee2bf49a0caa7f0c6d46cc094c87982249
chamodi <chamodipiyadasa@gmail.com>
2025-07-07 16:00:10 +0530

    skill section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
   
@@ -26,6 +27,7 @@ export const Home = () => {
 <main>
   <HeroSection/>
  <AboutSection />
+ <SkillsSection />
 </main>
 
 
